8bc827645cbf837b62a2e53ff6e4470a4c8b774a
Olga <data19101988@gmail.com>
2018-11-21 14:21:53 +0300

    add GET /todos

--- server/server.js
+++ server/server.js
@@ -19,7 +19,16 @@ app.post('/todos', (req, response) => {
   }, (e) => {
     response.status(400).send(e);
   })
-})
+});
+
+app.get('/todos', (req,res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
