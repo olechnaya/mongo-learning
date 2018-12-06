c8535bf8af4d198731f319f1c1a3e116fe1085c9
Olga <data19101988@gmail.com>
2018-12-07 01:10:15 +0300

    Added scss usage. Todo is added by html form

--- server/server.js
+++ server/server.js
@@ -4,11 +4,37 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {ObjectId} = require('mongodb');
+const hbs = require('hbs');
+// for partial views work properly
+// https://github.com/dpolivy/hbs-utils
+var hbsutils = require('hbs-utils')(hbs);
 
 var app = express();
 const port = process.env.PORT || 3000;
+const projectFolder = process.env.PWD;
+
+hbs.registerPartials(projectFolder + '/views/partials');
+hbsutils.registerWatchedPartials(projectFolder + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use(express.static(projectFolder + '/public'));
 
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/add-todo', (req, res) => {
+  res.render('add-todo.hbs', {
+    pageTitle: 'Adding to do',
+    welcomeMessage: 'Adding to do page'
+  });
+});
 
 app.post('/todos', (req, response) => {
   var todo = new Todo({
@@ -28,6 +54,22 @@ app.get('/todos', (req,res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+
+  if(ObjectId.isValid(id)) {
+    Todo.findById(id).then((todo) => {
+      res.send({todo});
+    }, (e) => {
+      res.status(400).send(e);
+    });
+  } else {
+    res.status(404).send(e);
+  }
+
+
 });
 
 
